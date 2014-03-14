8f0fca5230f4f5f1df07b2c21ab96bc692abfeb4
intrepid <mrferris@buffalo.edu>
2014-03-14 16:53:34 -0400

    Added more devices to health file

--- health/health.cpp
+++ health/health.cpp
@@ -16,6 +16,74 @@ int main(){
     }
     //check for out of bounds amperage or temperature levels and act accordingly.
     sleep(1);
+   
+
+
+    if(temp < min_temp_table[EPS]){
+      //send appropriate signal 
+    }
+    else if(temp > max_temp_table[EPS]){
+      //send appropriate signal 
+    }
+    //check for out of bounds amperage or temperature levels and act accordingly. 
+    sleep(1);
+
+
+
+    if(temp < min_temp_table[CAMERA]){
+      //send appropriate signal 
+    }
+    else if(temp > max_temp_table[CAMERA]){
+      //send appropriate signal
+    }
+    //check for out of bounds amperage or temperature levels and act accordingly. 
+    sleep(1);
+
+
+
+    if(temp < min_temp_table[LENS]){
+      //send appropriate signal
+    }
+    else if(temp > max_temp_table[LENS]){
+      //send appropriate signal
+    }
+    //check for out of bounds amperage or temperature levels and act accordingly. 
+    sleep(1);
+
+
+
+    if(temp < min_temp_table[RXN]){
+      //send appropriate signal
+    }
+    else if(temp > max_temp_table[RXN]){
+      //send appropriate signal
+    }
+    //check for out of bounds amperage or temperature levels and act accordingly. 
+    sleep(1);
+
+
+
+    if(temp < min_temp_table[TORQ]){
+      //send appropriate signal 
+    }
+    else if(temp > max_temp_table[TORQ]){
+      //send appropriate signal
+    }
+    //check for out of bounds amperage or temperature levels and act accordingly. 
+    sleep(1);
+
+
+
+    if(temp < min_temp_table[MAG]){
+      //send appropriate signal
+    }
+    else if(temp > max_temp_table[MAG]){
+      //send appropriate signal
+    }
+    //check for out of bounds amperage or temperature levels and act accordingly. 
+    sleep(1);
+
+
 
   }
   
