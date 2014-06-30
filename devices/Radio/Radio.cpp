1b9808a43b538b5e21bb603cf995a57e935d7bad
Michael Ferris <Mike@Michaels-MacBook-Air-4.local>
2014-06-30 13:06:27 -0400

    Receive functionality, builds correctly. Need to test.

--- devices/Radio/Radio.cpp
+++ devices/Radio/Radio.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <cstring>
 #include <pthread.h>
-#include "receive.h"
+//#include "receive.h"
 #include "packetgen.h"
 #include "serial.h"
 #include "Radio.h"
@@ -12,7 +12,9 @@ using namespace std;
 
 //int parsemessagetype(string mtype);
 
-int preparesourcestream( string ifilename, ifstream& source);
+//int preparesourcestream( string ifilename, ifstream& source);
+
+void wait_for(int fd_port, const unsigned char* text, size_t text_size);
 
 Radio::Radio(string path){
   
@@ -23,6 +25,22 @@ Radio::Radio(string path){
   device_path = path;
 
 }
+
+Packet Radio::receive(){
+  
+
+  if(!receive_queue.empty()){
+    Packet *packet = receive_queue.front();
+    receive_queue.pop();
+    return *packet;
+  }
+  else{
+    Packet packet = NULL;
+    return packet;
+  }
+
+}
+
 
 /*int Radio::configureRadio(string configurationFilePath){
 
@@ -41,11 +59,25 @@ Radio::Radio(string path){
   }*/
 
 int Radio::configure_radio(string configure_file_path){
-  
-  //TODO: open file, get payload bytes, put in packet, transmit.
+  const char* file = configure_file_path.c_str();
+  int fd = open(file,O_RDONLY);
+  if(fd < 0){
+    std::cerr << "Radio: configure file " << configure_file_path
+	      << " does not exist." << std::endl;
+    return -1;
+  }
+  unsigned char* configure_bytes;
+  int bytes_read = read(fd,configure_bytes,CONFIG_SIZE);
+  if(bytes_read < CONFIG_SIZE){
+    std::cerr << "Radio received invalid config file size." << std::endl;
+    return -1;
+  }
+  Packet *packet = new Packet(configure_bytes,SET_TRANSCEIVER_CONFIG,CONFIG_SIZE);
+  send_packet(packet);
   return 0;
 
 }
+
 
 int Radio::send_packet(Packet *packet){
   
@@ -87,10 +119,13 @@ int Radio::transmitPayload(string payloadFile){
 }
 */
 
-void *listenThread(void *x){
+
+void *listen_thread(void *x){
 
   string targetfile = "/dev/ttyS1";
- 
+
+  Radio *radio = (Radio*) x;
+
   int fd_port;
   fd_port = initPort(targetfile.c_str(), O_RDONLY);
   if(fd_port < 0){
@@ -99,56 +134,58 @@ void *listenThread(void *x){
   }
 
   unsigned char payload_size;
-  unsigned char packet[MAX_PACKET_LENGTH];
+  unsigned char packet_bytes[MAX_PACKET_LENGTH];
 
   while(true){
-    std::cout << "listening" << std::endl;
-    packet[0] = SYNC_1;
-    packet[1] = SYNC_2;
 
-    waitfor(fd_port, packet ,2);
-    std::cout << "saw packet" << std::endl;
-    //Next six bytes should be the rest of the header
-    //I don't know why this doesn't work, but it gets junk data: read(fd_port, packet+2, 6);
+    packet_bytes[0] = SYNC_1;
+    packet_bytes[1] = SYNC_2;
 
-    //let's try this one char at a time:
-    for(int i=2; i < 8; i++){ read(fd_port,packet+i,1); }
-
-    if(test_header_checksum(packet)){
-
-      if(has_payload(packet)){
-	payload_size = packet[5]; //put size_lower byte directly into payload_size
-	for(int i=0; i < (payload_size+2); i++){
-	  read(fd_port,packet+8+i,1); //read payload and its checksum
-	}
-	if(test_payload_checksum(packet+2,payload_size+6)){ //Trying checksum over header too
-#ifdef DEBUG
-	  write(1,packet,payload_size+10); //write the whole received packet to stdout
-#endif
-	  handle_payload(packet, payload_size);
-	} else {
-	  cerr << "Payload checksum failed.\n";
-	}
-
-      } else { //Stuff for packets with no payload
-	handle_command(packet);
-      }
-
-    } else { //Bad header
-      cerr << "Bad packet header.  I'm not part of your system!  I threw that packet on the ground!\n";
+    wait_for(fd_port, packet_bytes ,2);
+    int i;
+    for(i=2; i < 8; i++){
+      read(fd_port,packet_bytes+i,1);
     }
+    
+    if(packet_bytes[4] == 0){
+      payload_size = packet_bytes[5];
+      for(i=0;i<payload_size+2;i++){
+	read(fd_port,packet_bytes+8+i,1);
+      }
+    }
+    
+    Packet *packet = new Packet(packet_bytes);
+    radio->receive_queue.push(packet);
+    
+
   }
   close(fd_port);
   pthread_exit(NULL);
+
 }
+
 
 int Radio::listen(){
 
   pthread_t thread;
-  pthread_create(&thread, NULL, listenThread,NULL);
+  pthread_create(&thread, NULL, listen_thread,this);
   return 0;
 
 }
 
+
+void wait_for(int fd_port, const unsigned char* text, size_t text_size){
+  int i;
+  char foo;
+  for(i=0; i<text_size; i++){
+    while(1) {
+      read(fd_port, &foo, 1); //grab a character
+      if(foo == text[i]) //if it matches
+        break; //break out of the wait loop and move to the next char
+      else
+        i=0; //if it doesn't, go back to watching for the first char
+    }
+  }
+}
 
 
--- devices/Radio/Radio.h
+++ devices/Radio/Radio.h
@@ -11,6 +11,8 @@ using namespace std;
 #ifndef RADIO_H_
 #define RADIO_H_ 
 
+#define CONFIG_SIZE 34
+
 /*enum message_type{
 
   NO_OP_COMMAND,
@@ -33,64 +35,14 @@ class Radio{
   int listen();
   int transmit_payload(unsigned char payload[],unsigned char payload_size);
   int send_packet(Packet *packet);
-  
-  
+  std::queue<Packet*> receive_queue;
+  Packet receive();
 
  private:
   string device_path;
   int fd_target;
+
+
 };
-
-
-inline int parsemessagetype(string mtype){
-  if(mtype.length()>1){
-    cerr << "Invalid message type. Valid options are [ntcmRSF]\n";
-    return(-1);
-  } else if(mtype.length() == 0){
-    cerr << "No message type on command line. Using interactive transmit mode\n";
-    return(-2);
-  }
-
-  char mt = mtype.c_str()[0];
-  switch(mt){
-  case 'n': return(NO_OP_COMMAND);
-    break; //heh, like it matters. Yay habits.
-  case 'R': return(RESET_SYSTEM);
-    break;
-  case 't': return(TRANSMIT_DATA);
-    break;
-  case 'c': return(GET_TRANSCEIVER_CONFIG);
-    break;
-  case 'S': return(SET_TRANSCEIVER_CONFIG);
-    break;
-  case 'm': return(TELEMETRY_QUERY);
-    break;
-  case 'F': return(WRITE_FLASH);
-    break;
-  default:
-    cerr << "Invalid message type. Valid options are [ntcmRSF]\n";
-    return(-1);
-  }
-}
-
-inline int preparesourcestream( string ifilename, ifstream& source){
-
-  if( ifilename.compare("") == 0)
-    {
-      cerr << "No \"-i inputfilename\" given, using stdin\n";
-      cerr << "Enter payload, ^D to end: ";
-      ifilename = "/dev/stdin";
-    }
-
-  source.open(ifilename.c_str(), ios::in);
-
-  if(source.fail())
-    {
-      cerr << "Failed to open source file\n";
-      return(-1);
-    }
-  else return(0);
-}
-
 
 #endif
